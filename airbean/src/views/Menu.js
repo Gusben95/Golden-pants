0b000d05a418a2670872fcf5a7dba7ec8dd80420
Fanny Reslow <reslowf@gmail.com>
2022-03-25 12:02:03 +0100

    fetching menu

--- airbean/src/views/Menu.js
+++ airbean/src/views/Menu.js
@@ -1,4 +1,17 @@
 export default function Menu() {
+  // https://my-json-server.typicode.com/zocom-christoffer-wallenberg/airbean/menu
+
+  // get coffee menu [array] by fetch
+  async function getMenu() {
+    let response = await fetch(
+      "https://my-json-server.typicode.com/zocom-christoffer-wallenberg/airbean/menu"
+    );
+    let data = response.json();
+
+    console.log(data);
+  }
+  getMenu();
+
   return (
     <section>
       <h1>This is where the menu will be</h1>
