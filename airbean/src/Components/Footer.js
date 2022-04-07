5a9b3d08352427c1a11b0f2bd43d54c065a4f8c1
Fanny Reslow <reslowf@gmail.com>
2022-04-07 15:34:24 +0200

    adding footer

--- airbean/src/Components/Footer.js
+++ airbean/src/Components/Footer.js
@@ -1,3 +1,4 @@
+import "../views/stylesheets/footer.css";
 export default function Footer() {
   // a section for footer with background img in css
   return <section className="footer--section"></section>;
