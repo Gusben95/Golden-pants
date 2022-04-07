e75ec4bcf7787df4d3f0f7ec2bff4882840e376a
Gusben95 <bengtssongustav95@gmail.com>
2022-04-07 17:29:24 +0200

    Css updatering

--- airbean/src/Components/Navbar.js
+++ airbean/src/Components/Navbar.js
@@ -4,12 +4,23 @@ import Shopingcart from "./Shopingcart";
 import NavList from "./NavList";
 import "../views/stylesheets/NavBar.css"
 import bag from "../assets/graphics/bag.svg";
+import close from "../assets/graphics/close.svg"
+import open from "../assets/graphics/navicon.svg"
 // navbar takes 2 prop that determens if it should contain navlist or shopingcart(true or false)
 export default function Navbar({ navlist, shopingcart }) {
   const [hamburgerOpen, setHamburgerOpen] = useState(false);
   const [cartOpen, setCartOpen] = useState(false);
 
   // toggle -> maybe could be improved if  we have one function instead and call diffrent items to toggle
+  let icon = 0
+
+  if (hamburgerOpen === true) {
+     icon = close;
+  }
+else{
+  console.log(icon, "close")
+  icon = open;
+}
 
   const toggleHamburger = () => {
     setHamburgerOpen(!hamburgerOpen);
@@ -40,9 +51,13 @@ export default function Navbar({ navlist, shopingcart }) {
         {/*  if navlist is set to true then display icon/button */}
         {navlist && (
           <div className="navIcon" onClick={toggleHamburger}>
+            <div id="iconWrapper">
+           
             <div
-              className={hamburgerOpen ? "openBurger" : "closedBurger"}
-            ></div>
+              className={hamburgerOpen ?  "openBurger" : "closedBurger"}>
+              <img id="navImg"src={icon}></img>
+              </div>   
+            </div>
           </div>
         )}
       </div>
