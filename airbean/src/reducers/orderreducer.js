4719e02a7f44778b97ac5ca17e9a64cd32ed47f5
Fanny Reslow <reslowf@gmail.com>
2022-04-07 09:11:40 +0200

    fixing comment

--- airbean/src/reducers/orderreducer.js
+++ airbean/src/reducers/orderreducer.js
@@ -2,18 +2,9 @@
 let item = localStorage.getItem("cart");
 let itemParsed = JSON.parse(item);
 console.log(itemParsed);
-<<<<<<< HEAD
 if (!itemParsed) {
   itemParsed = [];
 }
-=======
-if (!itemParsed){
-  itemParsed = []
-}
-// // if (itemParsed === null) {
-// //   itemParsed = [];
-// // }
->>>>>>> 2e3056e7b1ea09f9dcd3146fef9e5d907f3baae7
 
 console.log(itemParsed);
 const initialState = {
