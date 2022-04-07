eace8ce39e163ed8b99f05cd1afa64e86f0e6b89
Fanny Reslow <reslowf@gmail.com>
2022-04-07 18:08:48 +0200

    Revert "Merge pull request #13 from Gusben95/Development"
    This reverts commit 557925d76064169a8c4b2a50ec316f45cd56ab09, reversing
    changes made to cd5bd67036e2637f2713c4fca655d1431acb6ea6.

--- airbean/src/reducers/orderreducer.js
+++ airbean/src/reducers/orderreducer.js
@@ -1,80 +1,48 @@
-//  setting initialstate beacuse when reload we want to start on whatever that is in localstorage
-let item = localStorage.getItem("cart");
-let itemParsed = JSON.parse(item);
-console.log(itemParsed);
-if (!itemParsed) {
-   itemParsed = [];
- }
+// import { addItemToOrder, increment, decrement } from "../actions/orderactions";
+// import Menu from "../views/Menu";
 
-console.log(itemParsed);
 const initialState = {
-  order: itemParsed,
+  order: [],
 };
 
 const orderreducer = (state = initialState, action) => {
-  // console.log(action.payload);
-  // console.log(`state ${JSON.stringify(state.order)}`);
+  console.log(action.payload);
+  console.log(state);
   switch (action.type) {
+    case "ADD_ITEM":
+      return {
+        ...state,
+        order: [...state.order, action.payload],
+      };
     case "INCREMENT":
-      const objectExist = state.order.some(
-        (item) => item.id === action.payload.id
-      );
-      if (objectExist) {
-        return {
-          ...state,
-          order: state.order.map((obj) => {
-            if (obj.id === action.payload.id) {
-              return {
-                ...obj,
-                amount: obj.amount + 1,
-              };
-            }
+      return {
+        ...state,
+        order: state.order.map((item) => {
+          if (item.id === action.payload.item.id) {
+            console.log("matchar!");
             return {
-              ...obj,
+              ...item,
+              amount: item.amount + action.payload.value,
             };
-          }),
-        };
-      }
-      return {
-        ...state,
-        order: [...state.order, action.payload],
+          }
+          console.log(" matchar ej");
+          return item;
+        }),
       };
-
     case "DECREMENT":
-      //  här vill jag kolla om amount är 1, tar man bort itemet från listan
-      // kanske kan flitrera bort itemet och returner nya array som listan
-      const objectExistD = state.order.some(
-        (item) => item.id === action.payload.id
-      );
-
-      const arr = [...state.order];
-      const newarr = arr.filter((item) => item.id !== action.payload.id);
-      console.log(newarr);
-      if (objectExistD) {
-        return {
-          ...state,
-          order: state.order.map((obj) => {
-            if (obj.id === action.payload.id && obj.amount > 1) {
-              return {
-                ...obj,
-                amount: obj.amount - 1,
-              };
-            } else if (obj.id === action.payload.id && obj.amount < 2)
-              return {
-                ...obj,
-                amount: 0,
-              };
-            return obj;
-          }),
-        };
-      }
       return {
         ...state,
-        order: [...state.order, action.payload],
+        order: state.order.map((item) => {
+          if (item.id === action.payload.item.id) {
+            return {
+              ...item,
+              amount: item.amount - action.payload.value,
+            };
+          }
+          return item;
+        }),
       };
-      // rensa köpet när man köpt
-      case "RESET":
-      return  initialState; 
+
     default:
       return state;
   }
